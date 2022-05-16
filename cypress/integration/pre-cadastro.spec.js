4597e7c14860fe6800d232950fc86d0287b1ac13
Adevanes Rosa <celebratte@gmail.com>
2022-05-16 12:09:38 -0300

    teste_ui

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -1,11 +1,26 @@
 ///<reference types="cypress"/>
 
+const faker = require('faker-br');
+
 describe('Funcionalidade Pré cadastro', () => {
     
-it('Deve completar o pré cadastro com sucesso', () => {
-    
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+});
 
-    
+
+
+it('Deve completar o pré cadastro com sucesso', () => {
+    cy.get('#reg_email').type(faker.internet.email())
+    cy.get('#reg_password').type('12345678')
+    cy.get(':nth-child(4) > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+    cy.get('#account_first_name').type('Luis')
+    cy.get('#account_last_name').type('Camerom')
+    cy.get('.woocommerce-Button').click()
+    cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+
 });
 
 
